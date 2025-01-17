e6faecd7582e7f2e880b4303dc074baa51e23710
chris-smekens-vinci <chris.smekens@student.vinci.be>
2025-01-17 17:19:10 +0100

    Updated function for the test contains the string "IPL" (case insensitive)

--- passwordChecker.js
+++ passwordChecker.js
@@ -2,7 +2,8 @@ export function passwordChecker(password) {
     const hasMinLength = password.length >= 8;
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
     const hasDigit = /\d/.test(password);
-    return hasMinLength && hasSpecialChar && hasDigit;
+    const doesNotContainIPL = !/ipl/i.test(password);
+    return hasMinLength && hasSpecialChar && hasDigit && doesNotContainIPL;
 }
 
 //module.exports = passwordChecker;
